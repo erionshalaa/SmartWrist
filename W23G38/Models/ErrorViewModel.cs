f5bbb6836ebe4a05f4c3c42671b5e31407b79560
erionshalaa <109277626+erionshalaa@users.noreply.github.com>
2023-11-18 19:24:47 +0100

    Add project files.

---
+++ W23G38/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace W23G38.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
